49148f14e72993ee086df7261653326317d44bce
Karan Bir <karan.bir@innroad.com>
2021-06-25 17:43:12 +0530

    Test functions added

---
+++ Calculate.cs
@@ -0,0 +1,30 @@
+﻿namespace Compcare.Test
+{
+	public class Calculate
+    {
+        public Calculate() { }
+        public int Add(int a, int b)
+        {
+            var result = a + b;
+            return result;
+        }
+
+        public int Substract(int a, int b)
+        {
+            var result = a - b;
+            return result;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            var result = a * b;
+            return result;
+        }
+
+        public float Divide(int a, int b)
+        {
+            var result = a / b;
+            return result;
+        }
+    }
+}
